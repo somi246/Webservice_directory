8b36076086975456accef1765bb000209aee04c4
Guanghui Li <gli@valdosta.edu>
2019-01-17 15:00:58 -0500

    created ManageController

---
+++ app/Http/Controllers/ManageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManageController extends Controller
+{
+    public function pending(){
+        return view('');
+    }
+    public function history(){
+        return view('');
+    }
+    public function allUser(){
+        return view('');
+    }
+}
