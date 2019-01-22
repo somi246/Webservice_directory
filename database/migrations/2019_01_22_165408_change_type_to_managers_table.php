e17189d76032ccf6f3dbd26c2610a70685ada89b
Guanghui Li <gli@valdosta.edu>
2019-01-22 11:59:51 -0500

    changed column type from string to integer for managers table

---
+++ database/migrations/2019_01_22_165408_change_type_to_managers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeTypeToManagersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('managers', function (Blueprint $table) {
+            $table->integer('user_id')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('managers', function (Blueprint $table) {
+            //
+        });
+    }
+}
